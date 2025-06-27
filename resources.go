55bb0d7c6b9621e2163e5e71ae32b9fa5d7eeb3c
Platon Ryzhikov <ihummer63@yandex.ru>
2025-06-27 15:59:44 +0300

    add full data structure from api

--- resources.go
+++ resources.go
@@ -57,22 +57,101 @@ var ResourceHTTPS2HTTP = map[bool]byte{
 }
 
 type ProtectionResource struct {
-	ID            int64
-	Name          string
-	ControlPanel  string
-	created       string
-	updated       string
-	Waf           bool
-	RedirectHTTPS bool
-	ServiceIP     string
-	BotProtection string
-	SSL           ResourceSSLType
-	SSLStatus     ResourceSSLStatus
-	GeoIPMode     string
-	GeoIPList     string
-	Https2Http    bool
-	WWWRedir      bool
-	status        ResourceStatus
-	enabled       bool
-	active        bool
+	active        bool              `json:"active"`
+	Waf           bool              `json:"is_waf_enabled"`
+	RedirectHTTPS bool              `json:"is_redirect_to_https_enabled"`
+	BotProtection byte              `json:"service_botprotect"`
+	SSL           ResourceSSLType   `json:"ssl_type"`
+	SSLStatus     ResourceSSLStatus `json:"service_ssl_status"`
+	GeoIPMode     byte              `json:"service_geoip_mode"`
+	GeoIPList     string            `json:"service_geoip_list"`
+	Https2Http    byte              `json:"service_https2http"`
+	WWWRedir      byte              `json:"service_wwwredir"`
+
+	aliases      []ResourceAlias      `json:"aliases"`
+	origins      []ResourceOrigin     `json:"origins"`
+	whitelists   []ResourceWhitelist  `json:"whitelists"`
+	blacklists   []ResourceBlacklist  `json:"blacklists"`
+	http_headers []ResourceHttpHeader `json:"http_headers"`
+
+	// read-only
+	ID           int64          `json:"id"`
+	enabled      bool           `json:"enabled"`
+	DomainName   string         `json:"name"`
+	status       ResourceStatus `json:"status"`
+	ControlPanel int64          `json:"client"`
+	created      string         `json:"created"`
+	updated      string         `json:"updated"`
+	ServiceIP    string         `json:"service_ip"`
+
+	TLSVersionsEnabled []string `json:"tls_versions_enabled"`
+
+	// internal parameters
+	ServiceCDN             int64  `json:"service_cdn"`
+	ServiceCDNHost         string `json:"service_cdn_host"`
+	ServiceCDNProxyHost    string `json:"service_cdn_proxy_host"`
+	ServiceEnable          int64  `json:"service_enable"`
+	ServiceForceSSL        int64  `json:"service_forcessl"`
+	ServiceGlobalWhitelist int64  `json:"service_global_whitelist"`
+	ServiceHTTP2           int64  `json:"service_http2"`
+	ServiceIPHash          int64  `json:"service_iphash"`
+	ServiceMethods         int64  `json:"service_methods"`
+	ServiceStream          int64  `json:"service_stream"`
+}
+
+type ResourceAlias struct {
+	Domain    string            `json:"alias_data"`
+	SSLExpire int64             `json:"alias_ssl_expire"`
+	SSL       ResourceSSLType   `json:"ssl_type"`
+	SSLStatus ResourceSSLStatus `json:"service_ssl_status"`
+
+	// read-only
+	ID      int64  `json:"id"`
+	created string `json:"alias_created"`
+	updated string `json:"alias_updated"`
+
+	// internal
+	AliasID int64 `json:"alias_id"`
+}
+
+type ResourceOrigin struct {
+	Data        string `json:"origin_data"`
+	Weight      byte   `json:"origin_weight"`
+	Mode        string `json:"origin_mode"`
+	FailTimeout int32  `json:"origin_fail_timeout"`
+	MaxFails    int32  `json:"origin_max_fails"`
+
+	// read-only
+	ID int64 `json:"id"`
+
+	// internal
+	OriginID int64 `json:"origin_id"`
+}
+
+type ResourceWhitelist struct {
+	Data string `json:"whitelist_data"`
+
+	// read-only
+	ID int64 `json:"id"`
+
+	// internal
+	WhitelistID int64 `json:"whitelist_id"`
+}
+
+type ResourceBlacklist struct {
+	Data string `json:"blacklist_data"`
+
+	// read-only
+	ID int64 `json:"id"`
+
+	// internal
+	BlacklistID int64 `json:"blacklist_id"`
+}
+
+type ResourceHTTPHeader struct {
+	key   string `json:"header_key"`
+	value string `json:"header_value"`
+
+	// read-only
+	ID int64 `json:"id"`
 }
